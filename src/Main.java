a0a38781853418bf2c728de977b11e18a2974b54
640610645 <nontapan_chanadee@cmu.ac.th>
2024-02-05 17:07:53 +0700

    implement Tonkenizer class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String constructionPlan =
+                "if(opponent % 5) then { invest 25 }" +
+                        "else move up";
+        Tokenizer tokenizer = new Tokenizer(constructionPlan);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        while (tokenizer.hasNextToken()) {
+            String token = tokenizer.consume();
+            System.out.println(token);
         }
     }
 }
