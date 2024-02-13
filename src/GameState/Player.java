090eb005fc137373b9137bea21b86f3eabf2d976
640610645 <nontapan_chanadee@cmu.ac.th>
2024-02-13 15:17:32 +0700

    เพิ่ม method collectInterest ของ Player

--- src/GameState/Player.java
+++ src/GameState/Player.java
@@ -12,10 +12,14 @@ public class Player {
     private Map<String, Long> bindings;
     private String plan;
     private List<Region> ownedRegion;
+    private long turns;
+    private long base;
+
     public Player(String name){
         this.name = name;
         bindings = new HashMap<>();
     }
+
     public void eval(){
         Parser p = new Parser(new Tokenizer(plan));
         p.parse(bindings);
@@ -28,6 +32,15 @@ public class Player {
     public long getBindings(String variable) {
         if(bindings.get(variable) != null) return bindings.get(variable);
         return 0;
+    }
+
+    public long collectInterest(){
+        long interest = 0;
+        for(Region r : ownedRegion){
+            double rate =  base * Math.log10(r.getDeposit()) * Math.log(turns);
+            interest += (long) (r.getDeposit() * rate / 100);
+        }
+        return interest;
     }
 
     public void addRegion(Region region){
--- src/GameState/Region.java
+++ src/GameState/Region.java
@@ -1,15 +1,21 @@
 package GameState;
+import java.lang.Math;
 
 public class Region {
     private final long row;
     private final long col;
     private long deposit;
     private Player ownner;
+    private Boolean isCityCenter;
 
     public Region(long row, long col, long deposit) {
         this.row = row;
         this.col = col;
         this.deposit = deposit;
     }
+    public long getDeposit(){
+        return deposit;
+    }
+
 
 }
